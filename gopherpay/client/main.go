38083a3170499303a6b6e9a881642abeed4e19b7
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-04-02 15:26:51 -0500

    new version using OOP

---
+++ gopherpay/client/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"OOP/gopherpay/payment"
+	"fmt"
+)
+
+func main() {
+	credit := payment.CreateCreditAccount(
+		"Debra Ingram",
+		"1111-2222-3333-4444",
+		5,
+		2021,
+		123)
+
+	fmt.Printf("Owner name: %v\n", credit.OwnerName())
+	fmt.Printf("Card Number: %v\n", credit.CardNumber())
+	fmt.Println("Trying to change card number")
+	err := credit.SetCardNumber("invalid")
+	if err != nil {
+		fmt.Printf("That didn't work: %v\n", err)
+	}
+	fmt.Printf("Available credit: %v\n", credit.AvailableCredit())
+}
