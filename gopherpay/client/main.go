2360b471731d7b7840f862f8805efc16d5dd8ada
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-04-06 16:30:21 -0500

    using channels

--- gopherpay/client/main.go
+++ gopherpay/client/main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
-	"../../gopherpay/payment"
+	"fmt"
 )
 
+type CreditAccount struct{}
+
+func (c *CreditAccount) processPayment(amount float32) {
+	fmt.Println("Processing credit card payment")
+}
+func CreateCreditAccount(chargeCh chan float32) *CreditAccount {
+	creditAccount := &CreditAccount{}
+	go func(chargeCh chan float32) {
+		for amount := range chargeCh {
+			creditAccount.processPayment(amount)
+		}
+	}(chargeCh)
+	return creditAccount
+}
 func main() {
-	var option payment.PaymentOption
 
-	option = payment.CreateCreditAccount(
-		"Debra Ingram",
-		"1111-2222-3333-4444",
-		5,
-		2021,
-		123)
+	chargeCh := make(chan float32)
+	CreateCreditAccount(chargeCh)
+	chargeCh <- 500
+	var a string
+	fmt.Scanln(&a)
+	// var option payment.PaymentOption
 
-	option.ProcessPayment(500)
+	// option = payment.CreateCreditAccount(
+	// 	"Debra Ingram",
+	// 	"1111-2222-3333-4444",
+	// 	5,
+	// 	2021,
+	// 	123)
 
-	option = payment.CreateCashAccount()
-	option.ProcessPayment(500)
+	// option.ProcessPayment(500)
+
+	// option = payment.CreateCashAccount()
+	// option.ProcessPayment(500)
 
 	// fmt.Printf("Owner name: %v\n", credit.OwnerName())
 	// fmt.Printf("Card Number: %v\n", credit.CardNumber())
