e92f224473cccd9e1a24dc4848e850fc324ab5a3
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-04-07 09:44:15 -0500

    using polymorphism

--- gopherpay/client/main.go
+++ gopherpay/client/main.go
@@ -1,32 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "../../gopherpay/payment"
 
-type CreditAccount struct{}
-
-func (c *CreditAccount) AvailableFunds() float32 {
-	fmt.Println("Getting credit funds")
-	return 250
-}
-
-type CheckingAccount struct{}
-
-func (c *CheckingAccount) AvailableFunds() float32 {
-	fmt.Println("Getting checking funds")
-	return 125
-}
-
-type HybridAccount struct {
-	CreditAccount
-	CheckingAccount
-}
-
-func (h *HybridAccount) AvailableFunds() float32 {
-	return h.CreditAccount.AvailableFunds() + h.CheckingAccount.AvailableFunds()
-}
 func main() {
-	ha := &HybridAccount{}
-	fmt.Println(ha.AvailableFunds())
+	var option payment.PaymentOption
+
+	option = &payment.CreditCard{}
+	option.ProcessPayment(500)
+
+	option = &payment.CheckingAccount{}
+
+	option.ProcessPayment(500)
 }
